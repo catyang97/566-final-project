7ce749cec9c1ed8bf3167f2c4a94cf0787a1142d
Catherine Yang <catyang@seas.upenn.edu>
2019-04-28 02:11:58 -0400

    paper is textured

--- src/glsl/paper1-frag.glsl
+++ src/glsl/paper1-frag.glsl
@@ -11,57 +11,39 @@ varying vec3 f_position;
 varying vec3 f_normal;
 varying vec2 f_uv;
 
-float random1( vec2 p , vec2 seed) {
-  return fract(sin(dot(p + seed, vec2(127.1, 311.7))) * 43758.5453);
-}
-
-// Worley-Perlin Noise from https://www.shadertoy.com/view/MdGSzt
-vec3 hash(vec3 p3) {
-	p3 = fract(p3 * vec3(0.1031, 0.11369, 0.13787));
-  p3 += dot(p3, p3.yxz + 20.0);
-  return -1.0 + 2.0 * fract(vec3((p3.x + p3.y) * p3.z, (p3.x + p3.z) * p3.y, (p3.y + p3.z) * p3.x));
-}
-
-float perlinNoise(vec3 point) {
-  vec3 pi = floor(point);
-  vec3 pf = point - pi;
-  vec3 w = pf * pf * (3.0 - 2.0 * pf);
-  
-  return 	mix(mix(mix(dot(pf - vec3(0, 0, 0), hash(pi + vec3(0, 0, 0))), 
-                      dot(pf - vec3(1, 0, 0), hash(pi + vec3(1, 0, 0))),
-                      w.x),
-                  mix(dot(pf - vec3(0, 0, 1), hash(pi + vec3(0, 0, 1))), 
-                      dot(pf - vec3(1, 0, 1), hash(pi + vec3(1, 0, 1))),
-                      w.x),
-                  w.z),
-              mix(mix(dot(pf - vec3(0, 1, 0), hash(pi + vec3(0, 1, 0))), 
-                      dot(pf - vec3(1, 1, 0), hash(pi + vec3(1, 1, 0))),
-                      w.x),
-                  mix(dot(pf - vec3(0, 1, 1), hash(pi + vec3(0, 1, 1))), 
-                      dot(pf - vec3(1, 1, 1), hash(pi + vec3(1, 1, 1))),
-                      w.x),
-                w.z),
-              w.y);
-}
-
-float getHeight(vec2 pos) {
-  float height = pow(1.0 - perlinNoise(vec3(pos.x, pos.y, 1000.0)), 0.3);
-  return height;
-}
-void main() {
-    vec4 color = vec4(u_albedo, 1.0);
+// Noise reference: https://www.shadertoy.com/view/XtsXRn
+float noise(vec3 x) {
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+    f = f*f*(3.-2.*f);
+	
+    float n = p.x + p.y*157. + 113.*p.z;
     
-    if (u_useTexture == 1) {
-        color = texture2D(texture, f_uv);
-    }
+    vec4 v1 = fract(753.5453123*sin(n + vec4(0., 1., 157., 158.)));
+    vec4 v2 = fract(753.5453123*sin(n + vec4(113., 114., 270., 271.)));
+    vec4 v3 = mix(v1, v2, f.z);
+    vec2 v4 = mix(v3.xy, v3.zw, f.y);
+    return mix(v4.x, v4.y, f.x);
+}
 
-    float d = clamp(dot(f_normal, normalize(u_lightPos - f_position)), 0.0, 1.0);
-    float height = getHeight(vec2(f_uv.x, f_uv.y))* 2.0;
+float fbm(vec3 p) {
+  p = mat3(0.28862355854826727, 0.6997227302779844, 0.6535170557707412,
+           0.06997493955670424, 0.6653237235314099, -0.7432683571499161,
+           -0.9548821651308448, 0.26025457467376617, 0.14306504491456504)*p;
+  return dot(vec4(noise(p), noise(p*2.), noise(p*4.), noise(p*8.)),
+             vec4(0.5, 0.25, 0.125, 0.06));
+}
 
-    float ran = random1(f_uv, vec2(0.2, 0.8));
-  
-    if (ran < 0.05) {
-        color = color * height; 
-    }
-    gl_FragColor = vec4(d * color.rgb * u_lightCol * u_lightIntensity + u_ambient, 1.0);
+void main() {
+    vec4 color = texture2D(texture, f_uv);
+    
+    // float d = clamp(dot(f_normal, normalize(u_lightPos - f_position)), 0.0, 1.0);
+    // float height = getHeight(vec2(f_uv.x, f_uv.y))* 2.0;
+
+    // gl_FragColor = vec4(d * color.rgb * u_lightCol * u_lightIntensity + u_ambient, 1.0);
+    vec2 uv = vec2(f_uv.x, f_uv.y);
+    vec3 col = 1.0 - 0.025 * vec3(smoothstep(0.6, 0.2, fbm(vec3(uv * 70.0,1.0))));
+    vec3 diff = vec3(color.x - (1.0-col.x)*2.0, color.y - (1.0-col.y)*2.0, color.z - (1.0-col.z)*2.0);
+
+    gl_FragColor = vec4(diff, 1.0);
 }
